aadb42f366fac344db377e21d7982cf3f76648d2
slava <213xixixi@gmail.com>
2023-06-13 20:31:27 +0300

    v2

--- currency/app/views.py
+++ currency/app/views.py
@@ -9,7 +9,8 @@ def index(request):
 
     if request.method == 'GET':
         context = {
-            'currency': currency
+            'currency': currency,
+            'converter': None
         }
         return render(
             request=request,
@@ -18,19 +19,27 @@ def index(request):
         )
 
     if request.method == 'POST':
-        amount_str = request.POST.get('amount')
-        try:
-            amount = float(amount_str)
-        except ValueError:
-            amount = 0.0
-        from_curr = request.POST.get('from')
-        to_curr = request.POST.get('to')
-        converter = round(
-            (currency[to_curr] / currency[from_curr]) * amount, 2)
-        context = {
-            'currency': currency,
-            'converter': converter
-        }
+        if 'convert' in request.POST:
+            amount_str = request.POST.get('amount')
+            try:
+                amount = float(amount_str)
+            except ValueError:
+                amount = 0.0
+            from_curr = request.POST.get('from')
+            to_curr = request.POST.get('to')
+            converter = round(
+                (currency[to_curr] / currency[from_curr]) * amount, 2)
+            context = {
+                'currency': currency,
+                'converter': converter,
+                'show_result': True
+            }
+        elif 'reset' in request.POST:
+            context = {
+                'currency': currency,
+                'converter': None,
+                'show_result': False
+            }
         return render(
             request=request,
             template_name='app_templates/index.html',
